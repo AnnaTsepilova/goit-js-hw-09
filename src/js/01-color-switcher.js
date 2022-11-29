cb005a3328bb096a9d3899f32d90aace95c39a4f
Anna Tsepilova <virgo.tsav@gmail.com>
2022-11-29 19:37:06 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,51 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function setBtnStatus(isStarted, btn) {
+    if (isStarted) {
+        btn.setAttribute("disabled", "disabled");
+    } else {
+        btn.removeAttribute("disabled");
+    };  
+    
+};
+
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+let intervalId = null;
+
+let isStarted = false; 
+ 
+startBtn.addEventListener('click', () => {
+    if (isStarted) {
+        return;
+    }
+        
+    isStarted = true;
+    setBtnStatus(isStarted, startBtn);
+    intervalId = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        document.body.style.backgroundColor = randomColor;
+    }, 1000);
+
+});
+
+stopBtn.addEventListener('click', () => {
+    isStarted = false;
+    setBtnStatus(isStarted, startBtn);
+    clearInterval(intervalId);
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
