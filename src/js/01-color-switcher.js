4242a65b44fa76d0c1f4ca0eed8b879f2aa5ea6d
Anna Tsepilova <virgo.tsav@gmail.com>
2022-11-29 23:33:09 +0200

    task-2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -49,3 +49,4 @@ stopBtn.addEventListener('click', () => {
 
 
 
+
---
+++ src/js/02-timer.js
@@ -0,0 +1,82 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const startBtn = document.querySelector("button[data-start]");
+const dateValue = document.querySelector("#datetime-picker");
+const daysField = document.querySelector("[data-days]");
+const hoursField = document.querySelector("[data-hours]");
+const minutesField = document.querySelector("[data-minutes]");
+const secondsField = document.querySelector("[data-seconds]");
+
+let intervalId = null;
+let deltaTime = null;
+let timeStampSelectedDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  
+  onClose(selectedDates) {
+    timeStampSelectedDate = selectedDates[0].getTime();
+    checkDate(selectedDates[0], startBtn);
+    deltaTime = timeStampSelectedDate - Date.now();
+  },
+};
+
+flatpickr(dateValue, options);
+
+function checkDate(date, btn) {
+    if (date.getTime() < Date.now()) {
+        btn.setAttribute("disabled", "disabled");
+        Notify.failure('Please choose a date in the future');
+    } else {
+        btn.removeAttribute("disabled");
+    }   
+}
+ 
+startBtn.addEventListener('click', () => {
+   startBtn.setAttribute("disabled", "disabled");
+    intervalId = setInterval(() => {
+        deltaTime -= 1000;
+
+        if (timeStampSelectedDate <= Date.now()) {
+            clearInterval(intervalId);
+        };
+
+        let { days, hours, minutes, seconds } = convertMs(deltaTime);
+        updateTimerFace( { days, hours, minutes, seconds } );
+    }, 1000);
+});
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateTimerFace( { days, hours, minutes, seconds } ) {
+    daysField.textContent = days;
+    hoursField.textContent = hours;
+    minutesField.textContent = minutes;
+    secondsField.textContent = seconds;
+}
+
+
+
+
+
